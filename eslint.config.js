75ccb9ea9305b790fa6d6d911d5166a1d35b8771
RITURAJ HARSHWARDHAN MISHRA <rm3313931@gmail.com>
2025-03-10 13:18:51 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  extends: ['eslint:recommended'],
+
+  rules: {
+
+    'no-unused-vars': 'error',
+
+    'semi': 'error',
+
+    'quotes': ['error', 'single'] // Enforce single quotes
+
+  }
+
+};
